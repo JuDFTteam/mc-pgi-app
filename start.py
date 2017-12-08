0abb31e33a60a5e051295ac5875e9119d1c2cbcf
broeder <j.broeder@fz-juelich.de>
2017-12-08 15:16:44 +0100

    some small changes, commenting out some features, making it ready for a short demonstration

--- start.py
+++ start.py
@@ -11,28 +11,47 @@ def get_start_widget(appbase, jupbase):
         <th style="width:70px" rowspan=2></th>
         <th style="text-align:center">Utility</th>
         <th style="width:70px" rowspan=2></th>
-        <th style="text-align:center">Workflow check</th>
         
     <tr>
     <td valign="top"><ul>
     <li><a href="{appbase}/fleur_tutorial/getting_started.ipynb" target="_blank">Fleur tutorial</a>
+    <li><a href="{appbase}/fleur_tutorial/getting_started.ipynb" target="_blank">KKR tutorial</a>
     </ul></td>
     
     <td valign="top"><ul>
-    <li><a href="{appbase}/corelevel_spectra/Plotting_corelevel_spectra_app.ipynb" target="_blank">all in one</a>
-    <li><a href="{appbase}/corelevel_spectra/display.ipynb" target="_blank">display</a>
-    <li><a href="{appbase}/corelevel_spectra/compare.ipynb" target="_blank">compare</a>
-    <li><a href="{appbase}/corelevel_spectra/search.ipynb" target="_blank">search</a>
+    <li><a href="{appbase}/corelevel_spectra/display.ipynb" target="_blank">Display Theoretical Spectra</a>
+    <li><a href="{appbase}/corelevel_spectra/search.ipynb" target="_blank">Find a Spectrum</a>
+    <li><a href="{appbase}/corelevel_spectra/search.ipynb" target="_blank">Fix Stoichiometry</a>
     </ul></td>
     
     <td valign="top"><ul>
-    <li><a href="{appbase}/slab/build.ipynb" target="_blank">Construct slab</a>
+    <li><a href="{appbase}/slab/build.ipynb" target="_blank">Construct slabs</a>
+    <li><a href="{appbase}/slab/build.ipynb" target="_blank">Construct slabs</a>
     </ul></td>
+
+    <tr>
+        <th style="text-align:center">Workflow check</th>
+        <th style="width:70px" rowspan=2></th>
+        <th style="text-align:center">Data Explorer</th>
+        <th style="width:70px" rowspan=2></th>
+    <tr>
 
     <td valign="top"><ul>
     <li><a href="{appbase}/slab/build.ipynb" target="_blank">fleur_scf</a>
-    <li><a href="{appbase}/slab/build.ipynb" target="_blank">fleur_eos</a>    
+    <li><a href="{appbase}/slab/build.ipynb" target="_blank">fleur_eos</a>
+    <li><a href="{appbase}/slab/build.ipynb" target="_blank">fleur_relax</a>
+    <li><a href="{appbase}/slab/build.ipynb" target="_blank">fleur_dos</a>
+    <li><a href="{appbase}/slab/build.ipynb" target="_blank">fleur_band</a>
     </ul></td>
+    
+    <td valign="top"><ul>
+    <li><a href="{appbase}/slab/build.ipynb" target="_blank">Find a simulation</a>
+    <li><a href="{appbase}/slab/build.ipynb" target="_blank">Structure finder</a>
+    <li><a href="{appbase}/slab/build.ipynb" target="_blank">Explorer Phase-space</a>
+    <li><a href="{appbase}/slab/build.ipynb" target="_blank">Convex hull of material class</a>
+    </ul></td>
+    
+    
     </tr></table>
 """
     
